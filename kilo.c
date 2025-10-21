8f51d0a84fb5e5c03aa15b5313b1a3d942e7fec7
LuffyTaro <kutty@POWERTRON>
2025-10-21 18:33:15 +0530

    Step18

--- kilo.c
+++ kilo.c
@@ -3,19 +3,26 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 
 
 
 struct termios orig_termios;
 
+void die(const char*s){
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode(){
-tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
+if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios)== -1)
+ die("tcsetattr");
 }
 
 void enableRawMode(){
 
 struct termios raw =orig_termios;
-tcgetattr(STDIN_FILENO,&orig_termios);
+if(tcgetattr(STDIN_FILENO,&orig_termios)==-1) die("tcgetattr");
 atexit(disableRawMode);
 raw.c_iflag &= ~(BRKINT| ICRNL| INPCK| ISTRIP | IXON);
 raw.c_oflag &= ~(OPOST);
@@ -23,7 +30,7 @@ raw.c_cflag &= (CS8); //bit mask with multiple bits
 raw.c_lflag &= (~ECHO|ICANON|IEXTEN | ISIG);
 raw.c_cc[VMIN] = 0;
 raw.c_cc[VTIME] = 1;
-tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw)==-1) die("tcsetattr");
 
 }
 
@@ -34,7 +41,7 @@ enableRawMode();
 while(1) {
 
 char c='\0';
-read(STDIN_FILENO, &c,1);
+if(read(STDIN_FILENO, &c,1) ==-1 && errno != EAGAIN) die("read");
 if(iscntrl(c)){
   printf("%d\r\n",c);
 } else {
