dfc6c49522eafcfa13a6ce634a114a21cff51512
LuffyTaro <kutty@POWERTRON>
2025-10-22 20:47:23 +0530

    Step 27

--- kilo.c
+++ kilo.c
@@ -5,28 +5,51 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+
+
+/***defines***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 /***data***/
-
+struct editorConfig{
 struct termios orig_termios;
+};
 
+struct editorConfig E;
 
 /***terminal***/
+
+int getWindowSize(int *rows,int *cols){
+   struct winsize ws;
+
+   if(ioctl(STDOUT_FILENO,TIOCGWINSZ, &ws )== -1 || ws.ws_col ==0){
+    return -1;
+   }else{
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+   }
+}
 void die(const char*s){
+  write(STDOUT_FILENO, "\x1b[2J",4); //to clear the screen//
+  write(STDOUT_FILENO, "/x1b[H" ,3); // command to position the cursor
     perror(s);
     exit(1);
 }
 
 void disableRawMode(){
-if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios)== -1)
+if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&E.orig_termios)== -1)
  die("tcsetattr");
 }
 
 void enableRawMode(){
 
-struct termios raw =orig_termios;
-if(tcgetattr(STDIN_FILENO,&orig_termios)==-1) die("tcgetattr");
+
+if(tcgetattr(STDIN_FILENO,&E.orig_termios)==-1) die("tcgetattr");
 atexit(disableRawMode);
+struct termios raw =E.orig_termios;
 raw.c_iflag &= ~(BRKINT| ICRNL| INPCK| ISTRIP | IXON);
 raw.c_oflag &= ~(OPOST);
 raw.c_cflag &= (CS8); //bit mask with multiple bits
@@ -37,20 +60,62 @@ if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw)==-1) die("tcsetattr");
 
 }
 
+
+char editorReadKey(){
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO,&c,1)) !=1){
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+
+/*** output ***/
+
+void editorDrawRows(){
+  int y;
+  for (y=0;y<24;y++){
+    write(STDOUT_FILENO,"~\r\n",3);
+  }
+}
+void editorRefreshScreen(){
+  write(STDOUT_FILENO, "\x1b[2J",4); //to clear the screen//
+  write(STDOUT_FILENO, "/x1b[H" ,3); // command to position the cursor
+  editorDrawRows();
+  write(STDOUT_FILENO,"/1xb[H",3);
+} 
+/*** input ***/ 
+
+void editorProcessKeypress(){
+  char c=editorReadKey();
+
+  switch(c){
+    case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J",4); //to clear the screen//
+      write(STDOUT_FILENO, "/x1b[H" ,3); // command to position the cursor
+      exit(0);
+      break;
+  }
+}
+
 /***init***/
+
 int main(){
-enableRawMode();
+// enableRawMode();
 
 while(1) {
-
-char c='\0';
-if(read(STDIN_FILENO, &c,1) ==-1 && errno != EAGAIN) die("read");
-if(iscntrl(c)){
-  printf("%d\r\n",c);
-} else {
-    printf("%d ('%c')\r\n" ,c ,c);
+editorRefreshScreen();
+editorProcessKeypress();
 }
-if (c == 'q') break;
-}
+// char c='\0';
+// if(read(STDIN_FILENO, &c,1) ==-1 && errno != EAGAIN) die("read");
+// if(iscntrl(c)){
+//   printf("%d\r\n",c);
+// } else {
+//     printf("%d ('%c')\r\n" ,c ,c);
+// }
+// if (c == CTRL_KEY('q')) break;
+// }
 return 0;
 }
